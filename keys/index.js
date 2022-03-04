c9cf8e731ec5bfaa087a22939102aebfeb53de0b
RuslanPostnikov <rp02081992@gmail.com>
2022-03-04 15:41:18 +0200

    Project

---
+++ keys/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    MONGODB_URI: `mongodb+srv://ruslan_postnikov:pNFNHx9RukQbyRSH@cluster0.ql2hy.mongodb.net/shop`,
+    SESSION_SECRET: 'some secret value',
+    SENDGRID_API_KEY: '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',
+    EMAIL_FROM: 'rp02081992@gmail.com',
+    BASE_URL: 'http://localhost:3000'
+}
+
+
