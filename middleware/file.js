ce8788a419fe2c584aee2168eebb88c21304a996
RuslanPostnikov <rp02081992@gmail.com>
2022-03-08 19:12:24 +0200

    Block 7. File uploading

--- middleware/file.js
+++ middleware/file.js
@@ -2,14 +2,14 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination(req, file, cb) {
-        cb(null, 'images')
+        cb(null, './images')
     },
     filename(req, file, cb) {
-        cb(null, new Date().toISOString() + '-' + file.originalname)
+        cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname)
     },
 });
 
-const allowedTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+const allowedTypes = ['image/png', 'image/jpg', 'image/jpeg', 'image/jfif'];
 
 const fileFilter = (req, file, cb) => {
     if(allowedTypes.includes(file.mimetype)) {
