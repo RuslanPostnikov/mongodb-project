74d9c846ea6cb537e47ae25750184ed1f658dc65
RuslanPostnikov <rp02081992@gmail.com>
2022-03-08 16:16:12 +0200

    Block 7. Sanitizers

--- utils/validators.js
+++ utils/validators.js
@@ -2,7 +2,9 @@ const {body} = require('express-validator');
 const User = require('../models/user');
 
 exports.registerValidators = [
-    body('email').isEmail().withMessage('Введите корректный email').custom(async (value, {req}) => {
+    body('email')
+        .isEmail().withMessage('Введите корректный email')
+        .custom(async (value, {req}) => {
         try {
             const user = await User.findOne({ email: value });
             if(user) {
@@ -11,13 +13,20 @@ exports.registerValidators = [
         } catch (e) {
             console.log(e);
         }
-    }),
-    body('password', 'Пароль должен быть минимум 6 символов').isLength({min: 6, max: 56}).isAlphanumeric(),
-    body('confirm').custom((value, {req}) => {
+    })
+        .normalizeEmail(),
+    body('password', 'Пароль должен быть минимум 6 символов')
+        .isLength({min: 6, max: 56}).isAlphanumeric()
+        .trim(),
+    body('confirm')
+        .custom((value, {req}) => {
         if(value !== req.body.password) {
             throw new Error('Пароли должны совптадать')
         }
         return true
-    }),
-    body('name').isLength({min: 3}).withMessage('Имя должно быть минимум 3 символа')
+    })
+        .trim(),
+    body('name')
+        .isLength({min: 3}).withMessage('Имя должно быть минимум 3 символа')
+        .trim()
 ]
