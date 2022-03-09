a9fc5d9c369f48e06f5fcf053e8f22455ff796ac
RuslanPostnikov <rp02081992@gmail.com>
2022-03-09 16:04:00 +0200

    Block 8. Heroku

--- index.js
+++ index.js
@@ -57,7 +57,12 @@ app.use(session({
 app.use(fileMiddleware.single('avatar'))
 app.use(csrf())
 app.use(flash())
-app.use(helmet())
+app.use(  helmet.contentSecurityPolicy({
+  useDefaults: true,
+  directives: {
+    "img-src": ["'self'", "https: data:"],
+    "script-src-elem": ["'self'", "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js", "'unsafe-inline'" ]   }
+}))
 app.use(compression())
 app.use(varMiddleware)
 app.use(userMiddleware)
