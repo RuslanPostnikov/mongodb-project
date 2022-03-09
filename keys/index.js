ed8af9062b254ba799eecfb819d11849e1886be5
RuslanPostnikov <rp02081992@gmail.com>
2022-03-09 15:18:39 +0200

    Block 8. Keys protection

--- keys/index.js
+++ keys/index.js
@@ -1,9 +1,5 @@
-module.exports = {
-    MONGODB_URI: `mongodb+srv://ruslan_postnikov:pNFNHx9RukQbyRSH@cluster0.ql2hy.mongodb.net/shop`,
-    SESSION_SECRET: 'some secret value',
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'rp02081992@gmail.com',
-    BASE_URL: 'http://localhost:3000'
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys.prod')
+} else {
+    module.exports = require('./keys.dev')
 }
-
-
